c6209aaf0e4e009747e72f08fd14be12c7f142f1
Tyler Durrett <hi@tylerdurrett.com>
2017-01-22 18:52:28 -0600

    Add shrink-to-fit=no

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- favicons etc -->
 	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/apple-icon-57x57.png">
@@ -25,16 +25,7 @@
 	<meta name="theme-color" content="#ffffff">
 
 	<?php wp_head();?>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-11112833-5', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
 </head>
 <body class="<?php body_class(); ?>">
 
