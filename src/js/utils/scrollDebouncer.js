bcf64b83427b8b970b0054fa560ca3e466a1b8a4
CalebBabin <CalebBabin@users.noreply.github.com>
2018-10-29 17:30:13 -0500

    updates scroll debouncer to fix scope issue

--- src/js/utils/scrollDebouncer.js
+++ src/js/utils/scrollDebouncer.js
@@ -2,16 +2,21 @@ class ScrollDebouncer {
     constructor ( callback ) {
 		this.callback = callback;
 		this.bounceCheck = false;
+		window.addEventListener('scroll', this.listener);
 	}
 	listener () {
 		if (!this.bounceCheck) {
-			window.requestAnimationFrame(this.frame);
+			window.requestAnimationFrame(()=>{this.frame});
 			this.bounceCheck = true;
 		}
 	}
 	frame () {
 		this.bounceCheck = false;
 		this.callback();
+	}
+	init (element = window) {
+		element.addEventListener('scroll', this.listener);
+		return this;
 	}
 }
 
