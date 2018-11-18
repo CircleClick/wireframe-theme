ee95bbb101fbae9d7098c3356ab4c706f9567dbf
CalebBabin <CalebBabin@users.noreply.github.com>
2018-11-18 12:12:24 -0600

    Kicks off new spacing practice

--- header.php
+++ header.php
@@ -6,13 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- favicons etc -->
-	<!--
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="256x256" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/favicon-256x256.png">
 	<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/favicon-32x32.png" sizes="32x32">
 	<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/favicon-16x16.png" sizes="16x16">
 	<link rel="manifest" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/manifest.json">
-	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/safari-pinned-tab.svg" color="#fc00ae">
-	<meta name="theme-color" content="#ffffff">-->
+	<meta name="theme-color" content="#ffffff">
+	<!--<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri();?>/assets/img/favicon/favicon.svg" color="#fc00ae">-->
 
 	<?php wp_head();?>
 
