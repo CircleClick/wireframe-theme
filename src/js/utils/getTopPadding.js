df4087ffb21b8c4b7b882236aa74d6ef3958a3f8
Caleb Babin <object.caleb@gmail.com>
2018-12-26 16:39:43 -0600

    resets CSS and JS

--- src/js/utils/getTopPadding.js
+++
@@ -1,6 +0,0 @@
-module.exports = (element) => {
-    var style = element.currentStyle || window.getComputedStyle(element),
-    margin = parseFloat(style.marginTop),
-    padding = parseFloat(style.paddingTop);
-    return margin+padding;
-}
