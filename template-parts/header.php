baad96c12891783cd48b54282c56119add0c5117
Caleb Babin <object.caleb@gmail.com>
2019-01-04 13:52:46 -0600

    adds basic helper classes

--- template-parts/header.php
+++ template-parts/header.php
@@ -3,14 +3,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<header id="site-header" class="site-header" role="banner">
+<div class="container fluid bg--white navbar--container" role="banner">
+	<div class="container">
+		<div class="row flex--center">
+			<div class="col-xs-9">
+				<?php the_custom_logo(); ?>
+			</div>
 
-	<div id="logo">
-		<?php the_custom_logo(); ?>
+			<div class="col-xs-3 text--right font--xl" role="navigation">
+				<i class="fas fa-bars" id="dropdown-switch"></i>
+			</div>
+		</div>
 	</div>
-
-	<nav id="top-menu" role="navigation">
-		<?php // top menu ?>
-	</nav>
-
-</header>
+</div>
