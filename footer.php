9a815552938e5998f55f5e79a355b968b2114da1
Caleb Babin <object.caleb@gmail.com>
2021-05-04 13:49:25 -0500

    use file time for version

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<script src="<?php echo get_stylesheet_directory_uri();?>/assets/js/main.min.js?v=3"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/main.js?v=<?php echo filemtime(get_stylesheet_directory() . '/assets/js/main.js'); ?>"></script>
 <?php wp_footer();?>
 </body>
 </html>
